6f65492f2429304337c8a980a613d62c7cd4dca0
Florian Marending <florian@marending.dev>
2023-10-22 17:11:35 +0200

    Add form on input for ff quick access

--- src/App.tsx
+++ src/App.tsx
@@ -31,14 +31,16 @@ const App: Component = () => {
       <Show when={!showConfig()} fallback={<Configuration />}>
         <div class="flex flex-col w-full p-2 md:p-4">
           <div class="w-full max-w-8xl mx-auto">
-            <input
-              type="text"
-              class="focus:outline-none w-full text-md placeholder:font-thin block mb-12 border-0 focus:ring-0"
-              placeholder="Go somewhere..."
-              autofocus
-              value={searchTerm()}
-              onChange={(event) => setSearchTerm(event?.currentTarget?.value)}
-            />
+            <form onSubmit={(event) => event.preventDefault()}>
+              <input
+                type="text"
+                class="focus:outline-none w-full text-md placeholder:font-thin block mb-12 border-0 focus:ring-0"
+                placeholder="Go somewhere..."
+                autofocus
+                value={searchTerm()}
+                onChange={(event) => setSearchTerm(event?.currentTarget?.value)}
+              />
+            </form>
             <div class="flex flex-col gap-2">
               <Show when={newLinkMode()}>
                 <NewLink onEditEnd={() => setNewLinkMode(false)} />
