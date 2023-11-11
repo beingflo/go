9cb5cb1d30b0999c89405f7969cf1e5b0d1006ec
Florian Marending <florian@marending.dev>
2023-11-11 19:37:37 +0100

    Clean up vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,9 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import solidSvg from "vite-plugin-solid-svg";
-// import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-  plugins: [
-    /* 
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-    // devtools(),
-    solidPlugin(),
-    solidSvg(),
-  ],
+  plugins: [solidPlugin(), solidSvg()],
   server: {
     port: 3000,
   },
